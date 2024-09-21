6ae4430b160b53780b4e0c26f64f684f9758f3e9
Stefan Olivier <s.olivier1194@gmail.com>
2024-09-21 22:48:08 +0200

    namespaced user API functions

--- src/library/formulas/utility.ts
+++ src/library/formulas/utility.ts
@@ -1,53 +1,16 @@
-import { type Locale, type LocalePair, makeLocalePair, verifyLocalePair } from "@src/library/locale/locale.ts";
+import { makeLocalePair, verifyLocalePair } from "@src/library/locale/locale.ts";
 import { PrefixTree } from "@src/library/containers/prefixTree.ts";
 import { LOCALE_PAIRS_MAPPING } from "@dictionaries";
-import { getDocumentValue } from "../interop/interop.ts";
-
-function GET_SHEET_GID(sheetName: string): number | null {
-    const sheet = SpreadsheetApp
-        .getActive()
-        .getSheetByName(sheetName);
-
-    if (sheet !== null)
-        return sheet.getSheetId();
-
-    return null;
-}
-
-function GET_STORE_VALUE(key: string): string | null {
-    return getDocumentValue(key);
-}
 
 /**
- * @param word
- * @param [sourceLocale="en-US"]
- * @param [targetLocal="en-GB"]
+ * Convert text from US dialect to UK dialect.
+ *
+ * @param {string} text The target text to be converted.
+ * @return The same input text with all US dialect words replaced with their UK equivalent.
  * @customfunction
  * */
-function TRANSPOSE_WORD(word: string,
-                        sourceLocale: Locale = "en-US",
-                        targetLocale: Locale = "en-GB"): string {
-    const localePair = makeLocalePair(sourceLocale, targetLocale);
-    verifyLocalePair(localePair);
-
-    const nodeRoot = LOCALE_PAIRS_MAPPING[localePair];
-    const prefixTree = new PrefixTree(nodeRoot);
-
-    const match = prefixTree.searchWord(word);
-    if (match === null) return word;
-    return match;
-}
-
-/**
- * @param text
- * @param [sourceLocale="en-US"]
- * @param [targetLocal="en-GB"]
- * @customfunction
- * */
-function TRANSPOSE_TEXT(text: string,
-                        sourceLocale: Locale = "en-US",
-                        targetLocale: Locale = "en-GB"): string {
-    const localePair = makeLocalePair(sourceLocale, targetLocale);
+function ADR_US_TO_UK(text: string): string {
+    const localePair = makeLocalePair("en-US", "en-GB");
     verifyLocalePair(localePair);
 
     const nodeRoot = LOCALE_PAIRS_MAPPING[localePair];
@@ -58,41 +21,183 @@ function TRANSPOSE_TEXT(text: string,
 }
 
 /**
- * @param text
- * @param [sourceLocale="en-US"]
- * @param [targetLocal="en-GB"]
+ * Convert text from UK dialect to US dialect.
+ *
+ * @param {string} text The target text to be converted.
+ * @return The same input text with all UK dialect words replaced with their US equivalent.
  * @customfunction
  * */
-function LIST_LOCALE_WORDS(text: string,
-                           showSource: boolean = false,
-                           sourceLocale: Locale = "en-US",
-                           targetLocale: Locale = "en-GB"): string {
-    const localePair = makeLocalePair(sourceLocale, targetLocale);
+function ADR_UK_TO_US(text: string): string {
+    const localePair = makeLocalePair("en-GB", "en-US");
     verifyLocalePair(localePair);
 
     const nodeRoot = LOCALE_PAIRS_MAPPING[localePair];
     const prefixTree = new PrefixTree(nodeRoot);
 
-    const allWords = prefixTree.searchAllWords(text, showSource);
+    const transposed = prefixTree.transposeText(text);
+    return transposed;
+}
+
+/**
+ * List only US dialect words and filter out all other words.
+ *
+ * @param {string} text The target text to be listed.
+ * @return A comma-separated list of US dialect words.
+ * @customfunction
+ * */
+function ADR_LIST_US(text: string): string {
+    const localePair = makeLocalePair("en-US", "en-GB");
+    verifyLocalePair(localePair);
+
+    const nodeRoot = LOCALE_PAIRS_MAPPING[localePair];
+    const prefixTree = new PrefixTree(nodeRoot);
+
+    const allWords = prefixTree.searchAllWords(text, true);
     return allWords.join(", ");
 }
 
 /**
- * @param text
- * @param [sourceLocale="en-US"]
- * @param [targetLocal="en-GB"]
+ * List only UK dialect words and filter out all other words.
+ *
+ * @param {string} text The target text to be listed.
+ * @return A comma-separated list of UK dialect words.
  * @customfunction
  * */
-function REVEAL_LOCALE_WORDS(text: string,
-                           showSource: boolean = false,
-                           sourceLocale: Locale = "en-US",
-                           targetLocale: Locale = "en-GB"): string {
-    const localePair = makeLocalePair(sourceLocale, targetLocale);
+function ADR_LIST_UK(text: string): string {
+    const localePair = makeLocalePair("en-GB", "en-US");
     verifyLocalePair(localePair);
 
     const nodeRoot = LOCALE_PAIRS_MAPPING[localePair];
     const prefixTree = new PrefixTree(nodeRoot);
 
-    const revealedText = prefixTree.revealAllWords(text, "_", showSource);
+    const allWords = prefixTree.searchAllWords(text, true);
+    return allWords.join(", ");
+}
+
+/**
+ * List only US dialect words and convert to UK dialect, then filter out all other words.
+ *
+ * @param {string} text The target text to be listed and converted.
+ * @return A comma-separated list of US dialect words converted UK dialect.
+ * @customfunction
+ * */
+function ADR_LIST_US_TO_UK(text: string): string {
+    const localePair = makeLocalePair("en-US", "en-GB");
+    verifyLocalePair(localePair);
+
+    const nodeRoot = LOCALE_PAIRS_MAPPING[localePair];
+    const prefixTree = new PrefixTree(nodeRoot);
+
+    const allWords = prefixTree.searchAllWords(text, false);
+    return allWords.join(", ");
+}
+
+/**
+ * List only UK dialect words and convert to US dialect, then filter out all other words.
+ *
+ * @param {string} text The target text to be listed and converted.
+ * @return A comma-separated list of UK dialect words converted US dialect.
+ * @customfunction
+ * */
+function ADR_LIST_UK_TO_US(text: string): string {
+    const localePair = makeLocalePair("en-GB", "en-US");
+    verifyLocalePair(localePair);
+
+    const nodeRoot = LOCALE_PAIRS_MAPPING[localePair];
+    const prefixTree = new PrefixTree(nodeRoot);
+
+    const allWords = prefixTree.searchAllWords(text, false);
+    return allWords.join(", ");
+}
+
+/**
+ * Reveal only US dialect words and replace all other words with a specified character.
+ *
+ * @param {string} text The target text to be revealed.
+ * @param {string} replaceOther The character to use for replacing non-US dialect words.
+ * @return The input text with all US dialect words revealed and all other words redacted.
+ * @customfunction
+ * */
+function ADR_REVEAL_US(text: string, replaceOther: string): string {
+    const localePair = makeLocalePair("en-US", "en-GB");
+    verifyLocalePair(localePair);
+
+    const nodeRoot = LOCALE_PAIRS_MAPPING[localePair];
+    const prefixTree = new PrefixTree(nodeRoot);
+
+    const replaceOtherChecked = (replaceOther === "")
+        ? "_"
+        : replaceOther.slice(0, 1);
+
+    const revealedText = prefixTree.revealAllWords(text, replaceOtherChecked, true);
+    return revealedText;
+}
+
+/**
+ * Reveal only UK dialect words and replace all other words with a specified character.
+ *
+ * @param {string} text The target text to be revealed.
+ * @param {string} replaceOther The character to use for replacing non-UK dialect words.
+ * @return The input text with all UK dialect words revealed and all other words redacted.
+ * @customfunction
+ * */
+function ADR_REVEAL_UK(text: string, replaceOther: string): string {
+    const localePair = makeLocalePair("en-GB", "en-US");
+    verifyLocalePair(localePair);
+
+    const nodeRoot = LOCALE_PAIRS_MAPPING[localePair];
+    const prefixTree = new PrefixTree(nodeRoot);
+
+    const replaceOtherChecked = (replaceOther === "")
+        ? "_"
+        : replaceOther.slice(0, 1);
+
+    const revealedText = prefixTree.revealAllWords(text, replaceOtherChecked, true);
+    return revealedText;
+}
+
+/**
+ * Reveal and convert US dialect words to UK dialect, then replace all other words with a specified character.
+ *
+ * @param {string} text The target text to be revealed and converted.
+ * @param {string} replaceOther The character to use for replacing non-US dialect words.
+ * @return The input text with all US dialect words revealed and converted to UK dialect, and all other words redacted.
+ * @customfunction
+ * */
+function ADR_REVEAL_US_TO_UK(text: string, replaceOther: string): string {
+    const localePair = makeLocalePair("en-US", "en-GB");
+    verifyLocalePair(localePair);
+
+    const nodeRoot = LOCALE_PAIRS_MAPPING[localePair];
+    const prefixTree = new PrefixTree(nodeRoot);
+
+    const replaceOtherChecked = (replaceOther === "")
+        ? "_"
+        : replaceOther.slice(0, 1);
+
+    const revealedText = prefixTree.revealAllWords(text, replaceOtherChecked, false);
+    return revealedText;
+}
+
+/**
+ * Reveal and convert UK dialect words to US dialect, then replace all other words with a specified character.
+ *
+ * @param {string} text The target text to be revealed and converted.
+ * @param {string} replaceOther The character to use for replacing non-UK dialect words.
+ * @return The input text with all UK dialect words revealed and converted to US dialect, and all other words redacted.
+ * @customfunction
+ * */
+function ADR_REVEAL_UK_TO_US(text: string, replaceOther: string): string {
+    const localePair = makeLocalePair("en-GB", "en-US");
+    verifyLocalePair(localePair);
+
+    const nodeRoot = LOCALE_PAIRS_MAPPING[localePair];
+    const prefixTree = new PrefixTree(nodeRoot);
+
+    const replaceOtherChecked = (replaceOther === "")
+        ? "_"
+        : replaceOther.slice(0, 1);
+
+    const revealedText = prefixTree.revealAllWords(text, replaceOtherChecked, false);
     return revealedText;
 }
